c1f5183aef5d2e7018ad3b8ec54911060552e6b5
Julian David Ramos Gomez <juli12319@hotmail.com>
2020-11-04 19:15:02 +0100

    trabajo Reglas jefe

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author CicloM
+ */
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Artículo 1. El jefe tiene la razon.");
+        System.out.println("Artículo 2. El jefe SIEMPRE tiena la razon.");
+        System.out.println("Artículo 3. En caso de duda se aplicará el articulo 2.");
+    }
+    
+}
